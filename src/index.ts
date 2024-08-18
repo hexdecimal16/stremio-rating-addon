51168e90306f7a27661a83abace3c7be51d90f4b
Dhairya Triapthi <dhairytripathi@gmail.com>
2024-08-18 20:12:51 +0530

    add: typescript support

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,50 @@
-const { addonBuilder, serveHTTP } = require('stremio-addon-sdk');
-const fs = require('fs');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const sharp = require('sharp');
-const { MovieDb } = require('moviedb-promise');
-const path = require('path');
+import { addonBuilder, serveHTTP, Args, MetaDetail } from 'stremio-addon-sdk';
+import * as fs from 'fs';
+import axios from 'axios';
+import sharp from 'sharp';
+import * as cheerio from 'cheerio';
+import * as dotenv from 'dotenv';
+import { MovieDb, DiscoverTvRequest, DiscoverMovieRequest, TvResult, MovieResult, TrendingRequest, PersonResult } from 'moviedb-promise';
+import * as path from 'path';
 
-const moviedb = new MovieDb('YOUR_TMDB_API_KEY', 'https://api.tmdb.org/3/');
 
-// Define your manifest
-const manifest = {
-    "id": "org.stremio.ratings",
-    "version": "1.0.0",
-    "name": "Ratings from Multiple Sources",
-    "description": "Adds ratings from IMDb, Rotten Tomatoes, Metacritic, Crunchyroll, Filmaffinity, Gadgets 360, and others to description and poster in Stremio.",
-    "resources": [
-        "catalog",
-        "meta"
-    ],
-    "types": ["movie", "series"],
-    "idPrefixes": ["tt"],
-    catalogs: [
-        {
-            id: 'trending',
-            name: 'Trending',
-            type: 'movie',
-            extra: []
-        },
-        {
-            id: 'trending',
-            name: 'Trending',
-            type: 'series',
-            extra: [{ name: 'search' }]
-        },
-        {
-            id: 'discover',
-            name: 'Discover',
-            type: 'movie',
-            extra: [
-                {
-                    name: 'genre', isRequired: false, options: [
-                        "popularity.asc",
-                        "popularity.desc",
-                        "release_date.asc",
-                        "release_date.desc",
-                        "revenue.asc",
-                        "revenue.desc",
-                        "primary_release_date.asc",
-                        "primary_release_date.desc",
-                        "original_title.asc",
-                        "original_title.desc",
-                        "vote_average.asc",
-                        "vote_average.desc",
-                        "vote_count.asc",
-                        "vote_count.desc"
-                    ]
-                }
-            ]
-        },
-        {
-            id: 'discover',
-            name: 'Discover',
-            type: 'series',
-            extra: [
-                {
-                    name: 'genre', isRequired: false, options: [
-                        "popularity.asc",
-                        "popularity.desc",
-                        "release_date.asc",
-                        "release_date.desc",
-                        "revenue.asc",
-                        "revenue.desc",
-                        "primary_release_date.asc",
-                        "primary_release_date.desc",
-                        "original_title.asc",
-                        "original_title.desc",
-                        "vote_average.asc",
-                        "vote_average.desc",
-                        "vote_count.asc",
-                        "vote_count.desc"
-                    ]
-                }
-            ]
-        },
-        {
-            id: 'search',
-            name: 'Search',
-            type: 'movie',
-            extra: [{ name: 'search', isRequired: true }]
-        },
-        {
-            id: 'search',
-            name: 'Search',
-            type: 'series',
-            extra: [{ name: 'search', isRequired: true }]
-        }
-    ]
-};
+// Load environment variables from .env file
+dotenv.config();
+
+// Get TMDB API key from environment variable
+const tmdbApiKey = process.env.TMDB_API_KEY;
+
+if (!tmdbApiKey) {
+    throw new Error('TMDB_API_KEY is not defined in the environment variables');
+}
+
+const moviedb = new MovieDb(tmdbApiKey, 'https://api.tmdb.org/3/');
+
+
+// Load the addon manifest
+const manifest = JSON.parse(fs.readFileSync(path.join(__dirname, '../manifest.json'), 'utf-8'));
 
 // Create a new addon builder
 const builder = new addonBuilder(manifest);
 
-async function addRatingToImage(base64String, ratingMap) {
+async function addRatingToImage(base64String: string, ratingMap: { [key: string]: string }): Promise<string> {
     try {
         // Remove base64 metadata and convert to buffer
         const base64Data = base64String.replace(/^data:image\/jpeg;base64,/, "");
         const imageBuffer = Buffer.from(base64Data, 'base64');
 
-        // Validate image format
-        await sharp(imageBuffer).metadata();
+        // Validate image using sharp
+        await sharp(imageBuffer).metadata()
+        
 
         const image = sharp(imageBuffer);
         const { width: imageWidth, height: imageHeight } = await image.metadata();
+
+        // Ensure image dimensions are found
+        if (!imageWidth || !imageHeight) {
+            console.error('Image dimensions not found');
+            return base64String;
+        }
 
         // Define SVG dimensions and padding
         const svgWidth = imageWidth;  // Set SVG width to the image width
@@ -126,13 +59,13 @@ async function addRatingToImage(base64String, ratingMap) {
         let ratingSvgs = '';
         let totalRatings = 0;
         for (const [key, value] of Object.entries(ratingMap)) {
-            let svgFilePath;
+            let svgFilePath: string | undefined;
             if (key === 'metacritic') {
                 svgFilePath = path.join(__dirname, 'assets', 'metacritic.svg');
             } else if (key === 'imdb') {
                 svgFilePath = path.join(__dirname, 'assets', 'imdb.svg');
             } else if (key === 'rotten_tomatoes') {
-                svgFilePath = value > 60
+                svgFilePath = value > '60'
                     ? path.join(__dirname, 'assets', 'rt_fresh.svg')
                     : path.join(__dirname, 'assets', 'rt_rotten.svg');
             }
@@ -189,25 +122,25 @@ async function addRatingToImage(base64String, ratingMap) {
         const modifiedBase64 = modifiedImageBuffer.toString('base64');
         return `data:image/jpeg;base64,${modifiedBase64}`;
     } catch (error) {
-        console.error('Error in addRatingToImage:', error.message);
+        console.error('Error in addRatingToImage:', (error as Error).message);
         // Return the original image if an error occurs
         return base64String;
     }
 }
 
-async function getMetadata(imdb, type) {
+async function getMetadata(imdb: string, type: string): Promise<MetaDetail> {
     try {
         const url = `https://v3-cinemeta.strem.io/meta/${type}/${imdb}.json`;
         const response = await axios.get(url);
         return response.data.meta;
     } catch (error) {
-        console.error(`Error fetching metadata: ${error.message}`);
-        return { title: 'N/A' };
+        console.error(`Error fetching metadata: ${(error as Error).message}`);
+        return {} as MetaDetail;
     }
 }
 
 // Scrape ratings and get posters
-async function scrapeRatings(imdbId, type) {
+async function scrapeRatings(imdbId: string, type: string): Promise<MetaDetail> {
     try {
         const metadata = await getMetadata(imdbId, type);
         const cleanTitle = metadata.name;
@@ -224,7 +157,7 @@ async function scrapeRatings(imdbId, type) {
 
         let ratingsDiv = $('div.Ap5OSd').first();
         let ratingText = '';
-        const ratingMap = {};
+        const ratingMap: { [key: string]: string } = {};
 
         if (!ratingsDiv) {
             console.error('Ratings div not found');
@@ -239,177 +172,217 @@ async function scrapeRatings(imdbId, type) {
                 score = score.split('/')[0];
             } else if (score.includes('%')) {
                 score = score.split('%')[0];
-            } else {
-                return;
             }
-            description += ` (${rating[1].trim()}: ${score})`;
-            ratingText += `${rating[1].trim()}: ${score} `;
             ratingMap[source] = score;
         });
+        // Add descriptions to metadata
+        const scrapeTitle = $('h2.LL6Nsc').first();
+        if (scrapeTitle && scrapeTitle.text()) {
+            description += `\nRating Source: ${scrapeTitle.text()}`;
+        }
 
-        const posterUrl = metadata.poster;
-        const posterResponse = await axios.get(posterUrl, { responseType: 'arraybuffer' });
-        const posterBase64 = `data:image/jpeg;base64,${Buffer.from(posterResponse.data).toString('base64')}`;
-        const posterWithRating = await addRatingToImage(posterBase64, ratingMap);
+        if (metadata.poster) {
+            const modifiedPoster = await addRatingToImage(metadata.poster, ratingMap);
+            metadata.poster = modifiedPoster;
+        }
 
-        metadata.poster = posterWithRating;
         metadata.description = description;
-
         return metadata;
+
     } catch (error) {
-        console.error(`Error fetching ratings: ${error.message}`);
-        return { title: 'N/A' };
+        console.error(`Error fetching ratings: ${(error as Error).message}`);
+        return {} as MetaDetail;
     }
 }
 
-// Define the meta handler for the addon
-builder.defineMetaHandler(async (args) => {
-    console.log('Received meta request:', args);
-    const imdbId = args.id;
-    const type = args.type;
-    const metadata = await scrapeRatings(imdbId, type);
-    console.log('Meta:', metadata);
+// Define the "meta" resource
+builder.defineMetaHandler(async (args: { id: string, type: string }) => {
+    const { id, type } = args;
+    let metadata: MetaDetail = {} as MetaDetail;
+    if (id.startsWith('tt')) {
+        const imdbId = id.split(':')[0];
+        metadata = await scrapeRatings(imdbId, type);
+    }
+
     return { meta: metadata };
 });
 
-async function trendingCatalog(type) {
-    const trendingRequest = {
-        media_type: type == 'movie' ? 'movie' : 'tv',
+
+
+// Fetch trending catalog
+async function trendingCatalog(type: string): Promise<any> {
+    const trendingRequest: TrendingRequest = {
+        media_type: type === 'movie' ? 'movie' : 'tv',
         time_window: 'week'
     };
 
     // Fetch trending data
     const response = await moviedb.trending(trendingRequest);
+    if (response.results == undefined) {
+        return { metas: [] };
+    }
 
     // Collect all the promises for fetching external IDs
-    const idPromises = response.results.map(async (curr) => {
+    const idPromises = response.results.map(async (curr: MovieResult | TvResult | PersonResult) => {
+        if (curr.id == undefined) {
+            return '';
+        }
         if (trendingRequest.media_type === 'movie') {
             const movieExternalResult = await moviedb.movieExternalIds({ id: curr.id });
+            if (movieExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return movieExternalResult.imdb_id;
         } else {
             const tvExternalResult = await moviedb.tvExternalIds({ id: curr.id });
+            if (tvExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return tvExternalResult.imdb_id;
         }
     });
 
     // Resolve all the promises concurrently
-    const ids = (await Promise.all(idPromises)).filter(id => id != '');
+    const ids = (await Promise.all(idPromises)).filter(id => id !== '');
     // Collect all the promises for scraping ratings
     const metaPromises = ids.map(id => scrapeRatings(id, type));
 
     // Resolve all the promises concurrently
     const metas = await Promise.all(metaPromises);
 
-    return Promise.resolve({ metas });
+    return { metas };
 }
 
-async function discoverCatalog(type, extra) {
-
-    page = 0
-    if (extra.skip) {
-        // if each page has 20 items, we can skip 20 items per page
+// Fetch discover catalog
+async function discoverCatalog(type: string, extra: any): Promise<any> {
+    let page = 0;
+    if (extra?.skip) {
         page = Math.floor(extra.skip / 20);
     }
-    let idPromises = [];
-    if (type == 'movie') {
+    let idPromises: Promise<string>[] = [];
+    if (type === 'movie') {
         const currentDate = new Date();
-        const discoverRequest = {
-            sort_by: extra.genre || 'popularity.desc',
-            with_runtime: {
-                gte: 30
-            },
-            release_date: {
-                lte: currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate()
-            },
+        const discoverRequest: DiscoverMovieRequest = {
+            sort_by: extra?.genre || 'popularity.desc',
+            "with_runtime.gte": 30,
+            "release_date.lte": `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`,
             page: page + 1
         };
 
-        console.log(discoverRequest);
-
         const response = await moviedb.discoverMovie(discoverRequest);
+        if (response.results == undefined) {
+            return { metas: [] };
+        }
 
-        idPromises = response.results.map(async (curr) => {
+        idPromises = response.results.map(async (curr: MovieResult) => {
+            if (curr.id == undefined) {
+                return '';
+            }
             const movieExternalResult = await moviedb.movieExternalIds({ id: curr.id });
+            if (movieExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return movieExternalResult.imdb_id;
         });
 
     } else {
-        const discoverRequest = {
-            sort_by: extra.genre || 'popularity.desc',
+        const discoverRequest: DiscoverTvRequest = {
+            sort_by: extra?.genre || 'popularity.desc',
             page: page + 1
         };
 
         const response = await moviedb.discoverTv(discoverRequest);
-        console.log(response);
+        if (response.results == undefined) {
+            return { metas: [] };
+        }
 
-        idPromises = response.results.map(async (curr) => {
+        idPromises = response.results.map(async (curr: TvResult) => {
+            if (curr.id == undefined) {
+                return '';
+            }
             const tvExternalResult = await moviedb.tvExternalIds({ id: curr.id });
+            if (tvExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return tvExternalResult.imdb_id;
         });
     }
 
     // Resolve all the promises concurrently
-    const ids = (await Promise.all(idPromises)).filter(id => id != '');
+    const ids = (await Promise.all(idPromises)).filter(id => id !== '');
     // Collect all the promises for scraping ratings
     const metaPromises = ids.map(id => scrapeRatings(id, type));
 
     // Resolve all the promises concurrently
     const metas = await Promise.all(metaPromises);
 
-    return Promise.resolve({ metas });
+    return { metas };
 }
 
-async function searchCatalog(type, extra) { 
-    console.log('Search:', extra.search);
-    query = extra.search;
+// Fetch search catalog
+async function searchCatalog(type: string, extra: any): Promise<any> {
+    console.log('Search:', extra?.search);
+    const query = extra?.search || '';
 
-    let idPromises = [];
-    if (type == 'movie') {
-        const response = await moviedb.searchMovie({ query: query });
-        console.log(response);
-
-        idPromises = response.results.map(async (curr) => {
+    let idPromises: Promise<string>[] = [];
+    if (type === 'movie') {
+        const response = await moviedb.searchMovie({ query });
+        if (response.results == undefined) {
+            return { metas: [] };
+        }
+        idPromises = response.results?.map(async (curr: MovieResult) => {
+            if (curr.id == undefined) {
+                return '';
+            }
             const movieExternalResult = await moviedb.movieExternalIds({ id: curr.id });
+            if (movieExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return movieExternalResult.imdb_id;
         });
 
     } else {
-        const response = await moviedb.searchTv({ query: query });
-        console.log(response);
-
-        idPromises = response.results.map(async (curr) => {
+        const response = await moviedb.searchTv({ query });
+        if (response.results == undefined) {
+            return { metas: [] };
+        }
+        idPromises = response.results.map(async (curr: TvResult) => {
+            if (curr.id == undefined) {
+                return '';
+            }
             const tvExternalResult = await moviedb.tvExternalIds({ id: curr.id });
+            if (tvExternalResult.imdb_id == undefined) {
+                return '';
+            }
             return tvExternalResult.imdb_id;
         });
     }
 
     // Resolve all the promises concurrently
-    const ids = (await Promise.all(idPromises)).filter(id => id != '');
+    const ids = (await Promise.all(idPromises)).filter(id => id !== '');
     // Collect all the promises for scraping ratings
     const metaPromises = ids.map(id => scrapeRatings(id, type));
 
     // Resolve all the promises concurrently
     const metas = await Promise.all(metaPromises);
 
-    return Promise.resolve({ metas });
+    return { metas };
 }
 
 // Define the catalog handler for the addon
-builder.defineCatalogHandler(async (args) => {
+builder.defineCatalogHandler(async (args: Args) => {
     console.log('Received catalog request:', args);
-    const type = args.type;
-    const catalogId = args.id;
+    const { type, id: catalogId, extra } = args;
 
     if (catalogId === 'trending') {
         return trendingCatalog(type);
     } else if (catalogId === 'discover') {
-        return discoverCatalog(type, args.extra);
+        return discoverCatalog(type, extra);
     } else if (catalogId === 'search') {
-        return searchCatalog(type, args.extra);
+        return searchCatalog(type, extra);
     }
-})
+});
 
-// Start the addon server
-serveHTTP(builder.getInterface(), { port: 10000 });
+// Start the HTTP server
+serveHTTP(builder.getInterface(), { port: 7000 });
 
-console.log('Addon running on http://localhost:10000');
