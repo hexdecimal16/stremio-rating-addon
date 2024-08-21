daa42bfa768475d52e46eee1ccd74d56f1c2cf4a
Dhairya Triapthi <dhairytripathi@gmail.com>
2024-08-22 05:16:55 +0530

    add: code for common_sense_media

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -83,6 +83,7 @@ async function generateRatingSVGs(imageWidth: number, imageHeight: number, ratin
 function getSVGFilePath(key: string, value: string) {
     if (key === 'metacritic') return path.join(__dirname, '../assets', 'metacritic.svg');
     if (key === 'imdb') return path.join(__dirname, '../assets', 'imdb.svg');
+    if (key === 'common_sense_media') return path.join(__dirname, '../assets', 'common_sense_media.svg');
     if (key === 'rotten_tomatoes') return value > '60'
         ? path.join(__dirname, '../assets', 'rt_fresh.svg')
         : path.join(__dirname, '../assets', 'rt_rotten.svg');
